98403ea9ffbd257c098122e327dbd8ed2ae01889
Issawat <actiondeveloper1998@gmail.com>
2024-01-07 15:30:01 +0700

    chore: setup

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const sampleData = ref({})
+
+const getSampleData = async () => {
+  const response = await fetch('http://localhost:3000')
+  sampleData.value = await response.json()
+}
+
+onMounted(() => {
+  getSampleData()
+})
+
+</script>
+
+<template>
+  <main>
+    {{ sampleData }}
+  </main>
+</template>
