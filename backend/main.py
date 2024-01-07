36909d92d40c1488ec0014af8376092c7a19150e
Issawat <actiondeveloper1998@gmail.com>
2024-01-07 19:49:54 +0700

    feat: frames api

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,13 @@ from pathlib import Path
 from fastapi import FastAPI, Response
 import base64
 import os
+import json
+from pydantic import BaseModel
+
+class TimestampReq(BaseModel):
+    timestamps: list[str]
+    ch: int
+    date: str
 
 app = FastAPI()
 
@@ -27,31 +34,34 @@ async def get_records():
     for record in os.listdir(base_path):
         if os.path.isdir(f"{base_path}/{record}"):
             records.append(record)
-    return Response({"records": records}, 200)
+    return Response(json.dumps({"records": records}), 200)
 
 
 @app.get("/frames-metadata")
 async def get_frames_metadata(date: str, ch: str):
     image_base_path = Path(f"{base_path}/{date}")
     file_timestamp_list = []
-    if image_base_path.exists():
-        file_timestamp_list = [x.split("_")[2] for x in os.listdir(image_base_path) if f'ch{ch}_{date}' in x]
     
-    return Response({"timestamps": file_timestamp_list, "ch": ch, "date": date}, 200)
+    reformat_date = date.replace("-", "")
+    
+    if image_base_path.exists():
+        file_timestamp_list = [x.split("_")[2] for x in os.listdir(image_base_path) if f'ch{ch}_{reformat_date}' in x]
+    
+    return Response(json.dumps({"timestamps": file_timestamp_list, "ch": ch, "date": date}), 200)
 
-@app.get("/frames")
-async def get_frames(timestamps: list[str], ch: int, date: str):
+@app.post("/frames")
+async def get_frames(timestampReq: TimestampReq):
     image_b64_list = []
     
-    if(len(timestamps) > 20):
-        return Response({"error": "too many frames requested"}, 400)
+    if(len(timestampReq.timestamps) > 20):
+        return Response(json.dumps({"error": "too many frames requested"}), 400)
     
-    for timestamp in timestamps:
-        image_path = Path(f"{base_path}/{date}/XVR_ch{ch}_{timestamp}_E.jpg")
+    for timestamp in timestampReq.timestamps:
+        image_path = Path(f"{base_path}/{timestampReq.date}/XVR_ch{timestampReq.ch}_{timestamp}_E.jpg")
         if image_path.exists():
             with open(image_path, "rb") as image_file:
                 base64_bytes = base64.b64encode(image_file.read())
                 base64_string = base64_bytes.decode()
                 image_b64_list.append(base64_string)
                 
-    return Response({"images": image_b64_list, "ch": ch, "date": date}, 200)
+    return Response(json.dumps({"images": image_b64_list, "ch": timestampReq.ch, "date": timestampReq.date}), 200)
