952db9e8ef800952444638a68d7d0f162602f728
Issawat <actiondeveloper1998@gmail.com>
2024-01-13 21:25:41 +0700

    style: label red color on indoor channels

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,6 +3,8 @@ import { RouterLink } from 'vue-router'
 import { Fetcher } from '../utils/fetcher'
 import { ref, onMounted, type Ref } from 'vue'
 
+const ATTENTION_CH = [2, 6, 7]
+
 type RecordEntry = {
   date: string
   availableChannels: number[]
@@ -32,7 +34,8 @@ onMounted(() => {
     <h1>CCTV object detection viewer</h1>
     <div v-for="record in records" :key="record.date">
       <h3>{{ record.date }}</h3>
-      <RouterLink class="button outline dark p-3" style="margin: 10px;" v-for="ch in record.availableChannels" :key="ch"
+      <RouterLink v-for="ch in record.availableChannels" :key="ch"
+        :class="`button ${ATTENTION_CH.includes(ch) ? 'error' : 'outline dark'} p-3`" style="margin: 10px;"
         :to="`/viewer?date=${record.date}&ch=${ch}`" target="_blank">
         Channel {{ ch }}
       </RouterLink>
